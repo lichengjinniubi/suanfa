ab6f1b20ffa87ef3663764181eb0047776045ba0
lichengjin <1197500583@qq.com>
2022-06-23 08:26:46 +0800

    dev

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,7 +14,20 @@ public class DemoApplication {
 
 		SpringApplication.run(DemoApplication.class, args);
 
-		System.out.println(8979877);
+		Map<String, String> map = new LinkedHashMap<>(10,1,true);
+		map.put("apple", "苹果");
+		map.put("watermelon", "西瓜");
+		map.put("banana", "香蕉");
+		map.put("peach", "桃子");
+
+		map.put("apple", "苹果2");
+
+		map.get("banana");
+		Iterator iter = map.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry entry = (Map.Entry) iter.next();
+			System.out.println(entry.getKey() + "=" + entry.getValue());
+		}
 	}
 
 }
